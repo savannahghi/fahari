676dc8ebe61759bad638741d0830701a5843554e
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-01 06:08:17 +0300

    Initialize Django Project

---
+++ pepfar_mle/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pepfar_mle.users.models import User
+from pepfar_mle.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
