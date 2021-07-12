2627f1d10119622beb04fd5a00f771492acb1b54
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-12 22:18:54 +0300

    fix: export cloud SQL proxy version

--- install_cloudsql_proxy.sh
+++ install_cloudsql_proxy.sh
@@ -6,7 +6,7 @@ set -o nounset
 
 export CLOUD_SQL_PROXY_VERSION=v1.23.0
 
-wget https://storage.googleapis.com/cloudsql-proxy/$CLOUD_SQL_PROXY_VERSION/cloud_sql_proxy.linux.amd64 -O /cloud_sql_proxy
-chmod +x /cloud_sql_proxy
+wget https://storage.googleapis.com/cloudsql-proxy/$CLOUD_SQL_PROXY_VERSION/cloud_sql_proxy.linux.amd64 -O /app/cloud_sql_proxy
+chmod +x /app/cloud_sql_proxy
 
 echo "Installed CloudSQL Proxy, version $CLOUD_SQL_PROXY_VERSION..."
