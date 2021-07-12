8bc28c968c5fbe200775ab29b6ec6cd2f21792fa
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-13 01:20:42 +0300

    fix: retire DATABASE_URL connection variable

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,6 @@ ALLOWED_HOSTS = env.list(
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
