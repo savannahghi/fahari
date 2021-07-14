a3f37259d219fe0a37d99ae0559c4126dacf7c2a
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-14 10:36:02 +0300

    fix: correct mailgun API URL for email sending

--- config/settings/production.py
+++ config/settings/production.py
@@ -99,7 +99,7 @@ EMAIL_SUBJECT_PREFIX = env(
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
-ADMIN_URL = env("DJANGO_ADMIN_URL")
+ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin/")
 
 # Anymail
 # ------------------------------------------------------------------------------
