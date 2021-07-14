110aee7592f6c2abd8f7d26bfa42a3f98a2a9636
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-14 10:54:10 +0300

    feat: edit default from email name

--- config/settings/production.py
+++ config/settings/production.py
@@ -86,7 +86,7 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Fahari ya Jamii M&E System <noreply@savannahghi.org>",
+    default="Fahari ya Jamii M&E System <mle@savannahghi.org>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
