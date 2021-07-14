ec7805d8f49d4988acba44cc76267b45a328298f
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-14 15:49:45 +0300

    fix: valid base template

--- pepfar_mle/static/js/project.js
+++ pepfar_mle/static/js/project.js
@@ -1,1 +1,9 @@
-/* Project specific Javascript goes here. */
+/* Google Analytics */
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-WW2W29ZMTZ");
