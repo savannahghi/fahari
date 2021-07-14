45d36dc3e16c03a044c4c8e2fe531c623d25957d
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-14 08:49:42 +0300

    fix: simplify entrypoint

--- config/settings/production.py
+++ config/settings/production.py
@@ -118,7 +118,7 @@ ANYMAIL = {
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_ENABLED
-COMPRESS_ENABLED = env.bool("COMPRESS_ENABLED", default=True)
+COMPRESS_ENABLED = True
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_URL
 COMPRESS_URL = STATIC_URL  # noqa F405
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
