990d7a0dc435ab6d849661a7c61027cf4ec0d1c6
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2021-07-20 09:03:23 +0300

    fix: catch validation errors in facility saving script

--- scripts/load_facilities.py
+++ scripts/load_facilities.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import django
 import pyexcel
+from django.core.exceptions import ValidationError
 
 
 def get_created_by_pk():
@@ -29,37 +30,40 @@ def load_facilities(source_path):
     count = len(records)
     for pos, r in enumerate(records):
         if r["Code"] is not None and r["Code"] != "None":
-            facility, created = Facility.objects.get_or_create(
-                name=r["Name"],
-                mfl_code=r["Code"],
-                defaults={
-                    "created_by": created_by,
-                    "updated_by": created_by,
-                    "organisation": org,
-                    "registration_number": r["Registration_number"],
-                    "keph_level": r["Keph level"],
-                    "facility_type": r["Facility type"],
-                    "facility_type_category": r["Facility_type_category"],
-                    "facility_owner": r["Owner"],
-                    "owner_type": r["Owner type"],
-                    "regulatory_body": r["Regulatory body"],
-                    "beds": int(r["Beds"]),
-                    "cots": int(r["Cots"]),
-                    "county": r["County"],
-                    "constituency": r["Constituency"],
-                    "sub_county": r["Sub county"],
-                    "ward": r["Ward"],
-                    "operation_status": r["Operation status"],
-                    "open_whole_day": r["Open_whole_day"] == "Yes",
-                    "open_public_holidays": r["Open_public_holidays"] == "Yes",
-                    "open_weekends": r["Open_weekends"] == "Yes",
-                    "open_late_night": r["Open_late_night"] == "Yes",
-                    "approved": r["Approved"] == "Yes",
-                    "public_visible": r["Public visible"] == "Yes",
-                    "closed": r["Closed"] == "Yes",
-                },
-            )
-            print(f"Facility: {facility}; Created: {created}; Pos {pos + 1}/{count}")
+            try:
+                facility, created = Facility.objects.get_or_create(
+                    name=r["Name"],
+                    mfl_code=r["Code"],
+                    defaults={
+                        "created_by": created_by,
+                        "updated_by": created_by,
+                        "organisation": org,
+                        "registration_number": r["Registration_number"],
+                        "keph_level": r["Keph level"],
+                        "facility_type": r["Facility type"],
+                        "facility_type_category": r["Facility_type_category"],
+                        "facility_owner": r["Owner"],
+                        "owner_type": r["Owner type"],
+                        "regulatory_body": r["Regulatory body"],
+                        "beds": int(r["Beds"]),
+                        "cots": int(r["Cots"]),
+                        "county": r["County"],
+                        "constituency": r["Constituency"],
+                        "sub_county": r["Sub county"],
+                        "ward": r["Ward"],
+                        "operation_status": r["Operation status"],
+                        "open_whole_day": r["Open_whole_day"] == "Yes",
+                        "open_public_holidays": r["Open_public_holidays"] == "Yes",
+                        "open_weekends": r["Open_weekends"] == "Yes",
+                        "open_late_night": r["Open_late_night"] == "Yes",
+                        "approved": r["Approved"] == "Yes",
+                        "public_visible": r["Public visible"] == "Yes",
+                        "closed": r["Closed"] == "Yes",
+                    },
+                )
+                print(f"Facility: {facility}; Created: {created}; Pos {pos + 1}/{count}")
+            except ValidationError as e:
+                print(f"can't save row {r}, got error {e}")
 
 
 if __name__ == "__main__":
