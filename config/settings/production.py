d8e7ce256f20e7a803a2696892f5fdc8c80361d8
saladgg <saladguyo60@gmail.com>
2021-09-27 17:03:58 +0300

    test deploy demo mle

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,10 +14,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
-    default=[
-        ".savannahghi.org",
-        "mle.savannahghi.org",
-    ],
+    default=[".savannahghi.org", "mle.savannahghi.org", "mle-demo.savannahghi.org"],
 )
 
 # DATABASES
