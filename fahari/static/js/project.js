9cdbf21ff20cd54cd1637d2a363cccab7b40686f
Kennedy Kori <kennedykori47@gmail.com>
2022-01-25 11:49:12 +0300

    chore: add more tests and other minor fixes

--- fahari/static/js/project.js
+++ fahari/static/js/project.js
@@ -1,3 +1,15 @@
+/**
+* Given an element, return the element's label text.
+*
+* This function only works if the given element has an `id` attribute and the
+* element's label has a `for` attribute whose value is the given element's
+* `id`. Otherwise, an empty string will be returned.
+*/
+function getElementLabelText($element) {
+    var element_id = $($element).attr("id");
+    return $(`label[for='${element_id}']`).text().trim();
+}
+
 (function($) {
     /* Google Analytics */
     window.dataLayer = window.dataLayer || [];
@@ -13,8 +25,17 @@
         $(".navbar-collapse").collapse("hide");
     });
 
-    // Initialize the bootstrap select plugin
-    $("select").selectpicker();
+    // Initialize the select2 plugin
+    $("select").each(function () {
+        $(this).select2({
+            allowClear: typeof $(this).data("allow-clear") !== "undefined" ? Boolean($(this).data("allow-clear")) : Boolean($(this).attr("multiple")),
+            closeOnSelect: !$(this).attr("multiple"),
+            placeholder: typeof $(this).data("placeholder") !== "undefined" ? $(this).data("placeholder") : {"id": `select2__placeholder__id__{$(this).attr('id')}`, "placeholder": `Select ${getElementLabelText(this)}`},
+            theme: "bootstrap4",
+            width: $(this).data("width") ? $(this).data("width") : $(this).hasClass("w-100") ? "100%" : "resolve",
+        });
+    });
 
+    // Initialize the date picker plugin
     $(".datepicker").datepicker();
 })(jQuery);
